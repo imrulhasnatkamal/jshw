dbf2097c988be871fa8bc743284e649b83ac438f
imrulhasnat kamal <54498171+imrulhasnatkamal@users.noreply.github.com>
2022-07-17 19:43:37 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,37 @@
+
+$(document).ready(function(){
+$('.c_btn').hover(function(){
+    alert('Showing you alert!');
+},
+function(){
+    alert('Showing you alert after leaving mouse');
+});
+
+// focus blur 
+$('input').focus(function(){
+    $(this).val('focused').css("background-color","red")
+});
+$('input').blur(function(){
+    $(this).val('blured').css("background-color","green")
+});
+
+// slide down 
+$('.c_btn2').click(function(){
+    $('.c_p2').slideDown();
+});
+// slide up 
+$('.c_btn3').click(function(){
+    $('.c_p3').slideUp();
+});
+
+// next function
+$('li').click(function(){
+    $(this).next().hide();
+});
+
+// parent 
+$('button').click(function(){
+    $(this).parent().css("background-color","green").css("border","4px solid red").css("color","blue");
+});
+
+});
